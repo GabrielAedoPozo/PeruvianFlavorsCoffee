a6158127ff8f6ef028be7f8d70eebb11f003a6f4
Gabriel Aedo <aedothegabriel@gmail.com>
2026-02-11 12:45:25 -0500

    Elimino el cloudfare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 // astro.config.mjs
 import { defineConfig } from 'astro/config';
-import cloudflare from '@astrojs/cloudflare';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://peruvianflavorscoffee.com',
-  integrations: [],
-  adapter: cloudflare(),
+  integrations: [sitemap()],
+  output: "static"
 });
